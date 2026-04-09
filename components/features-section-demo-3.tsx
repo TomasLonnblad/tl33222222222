0f6c51e2ca0b463a49e909623d9575206f914cb6
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-09 11:08:43 +0200

    x

--- components/features-section-demo-3.tsx
+++ components/features-section-demo-3.tsx
@@ -218,9 +218,54 @@ export const SkeletonTwo = () => {
   return (
     <div className="relative flex h-full flex-col items-start gap-10 overflow-hidden p-8">
       {/* TODO */}
-
-
+      <div className="-ml-20 flex flex-row">
+        {images.map((image, idx) => (
+          <motion.div
+            variants={imageVariants}
+            key={"images-first" + idx}
+            style={{
+              rotate: Math.random() * 20 - 10,
+            }}
+            whileHover="whileHover"
+            whileTap="whileTap"
+            className="mt-4 -mr-4 shrink-0 overflow-hidden rounded-xl border border-neutral-100 bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
+          >
+            <img
+              src={image}
+              alt="bali images"
+              width="500"
+              height="500"
+              className="h-20 w-20 shrink-0 rounded-lg object-cover md:h-40 md:w-40"
+            />
+          </motion.div>
+        ))}
       </div>
+      <div className="flex flex-row">
+        {images.map((image, idx) => (
+          <motion.div
+            key={"images-second" + idx}
+            style={{
+              rotate: Math.random() * 20 - 10,
+            }}
+            variants={imageVariants}
+            whileHover="whileHover"
+            whileTap="whileTap"
+            className="mt-4 -mr-4 shrink-0 overflow-hidden rounded-xl border border-neutral-100 bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
+          >
+            <img
+              src={image}
+              alt="bali images"
+              width="500"
+              height="500"
+              className="h-20 w-20 shrink-0 rounded-lg object-cover md:h-40 md:w-40"
+            />
+          </motion.div>
+        ))}
+      </div>
+
+      <div className="pointer-events-none absolute inset-y-0 left-0 z-[100] h-full w-20 bg-gradient-to-r from-white to-transparent dark:from-black" />
+      <div className="pointer-events-none absolute inset-y-0 right-0 z-[100] h-full w-20 bg-gradient-to-l from-white to-transparent dark:from-black" />
+    </div>
 
 
 
